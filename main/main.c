75e246d12d815b319324c5475e1fd117a0acb37d
Petteri Karjalainen <penes81@gmail.com>
2024-09-16 14:39:33 +0300

    Koodin aloittulu versio

---
+++ main/main.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+#include <inttypes.h>
+#include "esp_wifi.h" //Esp wifi library
+#include "esp_http_server.h" //http server for esp32
+
+
+void init_wifi();
+
+void app_main(void)
+{   
+    init_wifi();
+    printf("Hello world!\n");
+
+}
+
+
+void init_wifi(){
+    wifi_init_config_t wifi_init_default_config = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&wifi_init_default_config));
+}
+
+/*
+void start_http_server(){
+
+
+}
+
+
+void stop_http_server(){
+
+}
+*/
