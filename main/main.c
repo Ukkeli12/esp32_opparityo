03c4554982881b84edb4ee757bd69cc2429df056
Petteri Karjalainen <penes81@gmail.com>
2024-10-08 11:47:20 +0300

    Koodia lisätty

--- main/main.c
+++ main/main.c
@@ -7,7 +7,6 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
-#include "esp_wifi.h"
 #include "esp_event.h"
 #include "nvs_flash.h"
 #include "esp_log.h"
@@ -19,58 +18,98 @@
 #define esp_wifi_pass "Testisalasana"
 
 
-void init_wifi();
-
-void app_main(void)
-{   
-    init_wifi();
-    printf("Hello world!\n");
-
+/* Our URI handler function to be called during GET /uri request */
+esp_err_t get_handler(httpd_req_t *req)
+{
+    /* Send a simple response */
+    const char resp[] = "URI GET Response";
+    httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
 }
 
-
-
-static httpd_handle_t start_http_server(void)
+/* Our URI handler function to be called during POST /uri request */
+esp_err_t post_handler(httpd_req_t *req)
 {
-    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    /* Destination buffer for content of HTTP POST request.
+     * httpd_req_recv() accepts char* only, but content could
+     * as well be any binary data (needs type casting).
+     * In case of string data, null termination will be absent, and
+     * content length would give length of string */
+    char content[100];
 
-    // Start the HTTP server
-    ESP_LOGI(TAG, "Starting server on port %d", config.server_port);
-    httpd_handle_t server = NULL;
-    if (httpd_start(&server, &config) == ESP_OK) {
-        // Register URI handlers
-        httpd_register_uri_handler(server, &root_uri);
-        return server;
+    /* Truncate if content length larger than the buffer */
+    size_t recv_size = MIN(req->content_len, sizeof(content));
+
+    int ret = httpd_req_recv(req, content, recv_size);
+    if (ret <= 0) {  /* 0 return value indicates connection closed */
+        /* Check if timeout occurred */
+        if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
+            /* In case of timeout one can choose to retry calling
+             * httpd_req_recv(), but to keep it simple, here we
+             * respond with an HTTP 408 (Request Timeout) error */
+            httpd_resp_send_408(req);
+        }
+        /* In case of error, returning ESP_FAIL will
+         * ensure that the underlying socket is closed */
+        return ESP_FAIL;
     }
 
-    ESP_LOGI(TAG, "Error starting server!");
-    return NULL;
+    /* Send a simple response */
+    const char resp[] = "URI POST Response";
+    httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
 }
 
+/* URI handler structure for GET /uri */
+httpd_uri_t uri_get = {
+    .uri      = "/uri",
+    .method   = HTTP_GET,
+    .handler  = get_handler,
+    .user_ctx = NULL
+};
 
-void init_wifi(){
-    wifi_init_config_t wifi_init_default_config = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&wifi_init_default_config));
-
-    wifi_mode_t station_mode = WIFI_MODE_STA; 
-    ESP_ERROR_CHECK(esp_wifi_set_mode(station_mode));
-    wifi_config_t sta_config = {
-        .sta = {
-            .ssid = "Testi",
-            .password = "testisalasana"
-        
-        },
-        
-
-    };
-    sta_config.sta.bssid_set = false;
-
-    //esp_wifi_connect();
-}
-
+/* URI handler structure for POST /uri */
+httpd_uri_t uri_post = {
+    .uri      = "/uri",
+    .method   = HTTP_POST,
+    .handler  = post_handler,
+    .user_ctx = NULL
+};
 
 static void stop_http_server(httpd_handle_t server){
     if (server) {
         httpd_stop(server);
     }
 }
+
+httpd_handle_t start_webserver(void)
+{
+    /* Generate default configuration */
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+    /* Empty handle to esp_http_server */
+    httpd_handle_t server = NULL;
+
+    /* Start the httpd server */
+    if (httpd_start(&server, &config) == ESP_OK) {
+        /* Register URI handlers */
+        httpd_register_uri_handler(server, &uri_get);
+        httpd_register_uri_handler(server, &uri_post);
+    }
+    /* If server failed to start, handle will be NULL */
+    return server;
+}
+
+void app_main(void)
+{   
+    
+    printf("Hello world!\n");
+
+}
+
+
+
+
+
+
+
