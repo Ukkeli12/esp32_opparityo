fb7e243b37ef868cec74879abb6d72e9de555c82
Petteri Karjalainen <78467717+Ukkeli12@users.noreply.github.com>
2026-07-18 13:22:09 +0300

    Update main.c

--- main/main.c
+++ main/main.c
@@ -19,8 +19,8 @@
 #include "mbedtls/base64.h"
 
 #define relay_port GPIO_NUM_32 //Port of reley that used in program
-#define wifi_ssid "Jotakin" //ssid for esp wifi
-#define wifi_password "ToomiHan0" //Password for esp wifi
+#define wifi_ssid "test" //ssid for esp wifi
+#define wifi_password "test" //Password for esp wifi
 
 //#define school_setup //school wifi setup
 
